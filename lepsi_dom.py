e055e419a8fba7805c6e4934cf37b8c1ba69e447
niekto458 <tus.adam13@gmail.com>
2025-12-11 16:12:36 +0100

    dedina (3 domy)

--- lepsi_dom.py
+++ lepsi_dom.py
@@ -5,37 +5,39 @@ def build_house():
 
     start = player.position()
 
-    for x in range(width):
+    for i in range(3):
+        offset = positions.add(start, world(i * (width + 3), 0, 0))
+
+        for x in range(width):
             for z in range(length):
                 for y in range(height):
-                    if x == 0 or x == width - 1 \
-                    or z == 0 or z == length - 1:
-                        blocks.place(Block.STRIPPED_DARK_OAK_WOOD,\
-                        positions.add(start, world(x, y, z)))
+                    if x == 0 or x == width - 1 or z == 0 or z == length - 1:
+                        blocks.place(
+                            Block.STRIPPED_DARK_OAK_WOOD,
+                            positions.add(offset, world(x, y, z))
+                        )
 
-    blocks.fill(
+        blocks.fill(
             Block.AIR,
-            positions.add(start, world(1, 1, 1)),
-            positions.add(start, world(width - 2, height - 1,\
-            length - 2)),
+            positions.add(offset, world(1, 1, 1)),
+            positions.add(offset, world(width - 2, height - 1, length - 2)),
             FillOperation.REPLACE
         )
-    for x in range(width):
+
+        for x in range(width):
             for z in range(length):
                 blocks.place(
                     PLANKS_DARK_OAK,
-                    positions.add(start, world(x, -1, z))
+                    positions.add(offset, world(x, -1, z))
                 )
 
-    blocks.place(Block.OAK_DOOR, positions.add\
-        (start, world(2, 0, 0)))
+        blocks.place(Block.OAK_DOOR, positions.add(offset, world(2, 0, 0)))
 
-    
-    
-    for x in range(width):
+        for x in range(width):
             for z in range(length):
-                blocks.place(Block.SMOOTH_STONE_SLAB,\
-                    positions.add(start, world(x, height, z)))
+                blocks.place(
+                    Block.SMOOTH_STONE_SLAB,
+                    positions.add(offset, world(x, height, z))
+                )
 
-    
 player.on_chat("dom", build_house)
